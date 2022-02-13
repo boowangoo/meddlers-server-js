166ae907944a342b79c222d41092c8374e8c6418
boowangoo <bfwang@uwaterloo.ca>
2022-02-13 14:57:49 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const static = require("node-static");
+const file = new static.Server("./");
+const server = http.createServer((req, res) => {
+    req.addListener("end", () => {
+        file.serve(req, res);
+    }).resume();
+});
+const port = 3425;
+const ip = "localhost"
+
+server.listen(port, () => {
+    console.log(`Server is running at http://${ip}:${port}`);
+});
